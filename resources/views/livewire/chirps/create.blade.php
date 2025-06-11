7351a0c4205ac2415838ac09c32974491f4a2f01
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2025-06-11 16:59:14 +0800

    store

--- resources/views/livewire/chirps/create.blade.php
+++ resources/views/livewire/chirps/create.blade.php
@@ -4,7 +4,17 @@ use Livewire\Attributes\Validate;
 use Livewire\Volt\Component;
 
 new class extends Component {
+    #[Validate('required|string|max:255')]
     public string $message = '';
+
+    public function store(): void
+    {
+        $validated = $this->validate();
+
+        auth()->user()->chirps()->create($validated);
+
+        $this->message = '';
+    }
 }; ?>
 
 <div>
@@ -13,7 +23,7 @@ new class extends Component {
             wire:model="message"
             placeholder="{{ __('What\'s on your mind?') }}"
             class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">></textarea>
-<x-input-error :messages=" $errors->get('message')" class="mt-2" />
-<x-primary-button class="mt-4">{{ __('Chirp') }}</x-primary-button>
-</form>
+        <x-input-error :messages=" $errors->get('message')" class="mt-2" />
+        <x-primary-button class="mt-4">{{ __('Chirp') }}</x-primary-button>
+    </form>
 </div>
